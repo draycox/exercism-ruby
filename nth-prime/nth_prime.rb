02a4b0025da68ff8e2df89f322161b951034c0ae
Daniel Cox <draycox@gmail.com>
2016-01-13 21:00:19 -0600

    More challenges

---
+++ nth-prime/nth_prime.rb
@@ -0,0 +1,11 @@
+class Prime
+
+  def nth integer
+    raise ArgumentError if integer  < 1
+    upper_limit = integer * (Math.log(integer) + 3)
+    primes = (2..upper_limit).to_a
+    primes.each {|num| primes.delete_if {|prime| prime > num && (prime % num) == 0} }
+    primes[integer-1]  
+  end
+
+end
