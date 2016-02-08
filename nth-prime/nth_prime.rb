4987d5ec37fe7875e8747abadbdedf41f29e076d
Daniel Cox <draycox@gmail.com>
2016-02-08 09:08:06 -0600

    Fix prime push to github

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -5,7 +5,7 @@ class Prime
     upper_limit = integer * (Math.log(integer) + 3)
     primes = (2..upper_limit).to_a
     primes.each {|num| primes.delete_if {|prime| prime > num && (prime % num) == 0} }
-    primes[integer-1]  
+    primes[integer-1]
   end
 
 end
